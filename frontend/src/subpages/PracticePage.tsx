30ae64cccfc1f99f776eba448a9dc69408dd21c7
hotelFabie <fabianwikman@gmail.com>
2026-07-19 17:19:50 +0200

    introduction of session storage, question now is where we erase the value to not retain same cards eternally.

--- frontend/src/subpages/PracticePage.tsx
+++ frontend/src/subpages/PracticePage.tsx
@@ -1,16 +1,9 @@
 import { useLocation } from "react-router";
 import { useEffect, useState, useRef } from 'react'
 import Footer from '../components/Footer.tsx';
+import { useNavigate } from 'react-router';
 
-//wip: removal or update!!!
-/*type ResponseData = {
-  limit : number;
-  offset : number;
-  total : number;
-  words : Word[];
-}*/
-
-//data fetched from api
+//Representation of the specific data you get from fetching the JLPT API.
 type ApiData = {
   word : string;
   meaning: string;
@@ -20,21 +13,23 @@ type ApiData = {
 
 }
 
-//with custom data
+//WIP: Not sure just yet if this variable is of use; depends on what we want to show in the Review page.
 type Card = ApiData & {
-    isCorrectOnFirstTry : boolean;
+    hasCorrectUserAnswer : boolean;
 }
 
 export function PracticePage() {
     const textInput = useRef<HTMLInputElement>(null);
 
+    //WIP: need to add a state to all the cards that they are accepted.
     const [cards, setCards] = useState<Card[]>([]);
+    const [isLoading, setIsLoading] = useState<Boolean>(true);
+    const isComplete : boolean = !isLoading && cards.every(card => card.hasCorrectUserAnswer === true); //
 
     const location = useLocation();
     const chosenLevels = location.state.levels;
 
-    //the word should not literally be a word, it should have the thing under it.
-    //const currentCard : Card = cards[0];
+    const navigate = useNavigate();
     
     console.log("retrieved levels: ", chosenLevels);
 
@@ -44,7 +39,7 @@ export function PracticePage() {
         const randomCards : Card[] = []; 
         for (let i: number = 0; i < 10; i++) {
             let randomIndex = Math.floor(Math.random() * data.length);
-            const selectedWord: Card = { ...data[randomIndex], isCorrectOnFirstTry: true };
+            const selectedWord: Card = { ...data[randomIndex], hasCorrectUserAnswer: false };
             randomCards.push(selectedWord);
             console.log('this is the data entry: ', data[randomIndex]);
             data.splice(randomIndex, 1); //gotta check if it works. this is just not to get duplicates.
@@ -52,37 +47,53 @@ export function PracticePage() {
         return randomCards;
     }
 
+    
+
     useEffect(() => {
+        var responses;
+        
         console.log("useEffect triggered.");
 
-        //will not even need this?
-        //let cards: Card[] = [];
+        //no point in fetching if this is true.
+        if (isComplete) {
+            navigate("/review");
+            return;
+        }
 
         //this is a promise remake of what i am having under this construction.
         async function loadCards() {
-            //no direct data type for json available without aliasing.
-            const responses = await Promise.all(
-                chosenLevels.map((l: number) =>
-                    fetch(`https://jlpt-vocab-api.vercel.app/api/words/all?level=${l}`)
-                        .then((res: Response) => res.json())
-                )
-            );
+            const ssCards: string | null = sessionStorage.getItem('cards');
 
-            console.log(responses);
-            
-            //this may be a bit ugly...
-            const cards : Card[] = responses.flatMap((data: ApiData[]) => retrieveRandomCards(data)); //use the function here.
-        
-            setCards(cards);
+            //only do this fetch and all of the setters if so needed.
+            if (ssCards !== null) {
+                const cards = JSON.parse(ssCards);
+                setCards(cards);
+                console.log("session storage values: ", cards);
+            } else {
+                //everything below here.
+                responses = await Promise.all(
+                    chosenLevels.map((l: number) =>
+                        fetch(`https://jlpt-vocab-api.vercel.app/api/words/all?level=${l}`)
+                            .then((res: Response) => res.json())
+                    )
+                );
+
+                console.log(responses);
+
+                const cards: Card[] = responses.flatMap((data: ApiData[]) => retrieveRandomCards(data));
+
+                sessionStorage.setItem("cards", JSON.stringify(cards));
+
+                setCards(cards);
+            }
+            setIsLoading(false);
         }
 
-        loadCards();    
-    }, [chosenLevels]);
+        loadCards();
+
+    }, [chosenLevels]); 
 
 
-    //<char instead? not sure...>
-    //useRef, because passing like 'value={...}' will make the content 
-    //impossible for the user to erase. 
     function addChouonpuLetter(letter : string) : void {
         if (textInput.current !== null) {
             textInput.current.value += letter;
@@ -90,20 +101,24 @@ export function PracticePage() {
         }
     }
 
-    //necessary to have the "React."? maybe possible to retrieve this instead?
+    //necessary to have the "React."? can maybe retrieve KeyboardEvent directly.
     //https://stackoverflow.com/questions/38096687/how-can-i-remove-the-first-element-of-an-array-and-return-the-rest
     const keyDown = (e : React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key !== "Enter") {
             return;
         }   
         
-        //perhaps that we save and convert the value to small letters here?
+        //is correct should be true here. 
 
         if (e.currentTarget.value === cards[0].romaji) {
+            //we will probably need to make sure to set the boolean right here.
+            cards[0].hasCorrectUserAnswer = true; //just testing rn, maybe this is too exposed.
             const [, ...remainingCards] = cards;
+
             setCards(remainingCards);
             console.log("The answer given by the user was correct!");
             e.currentTarget.value = ""; //should be done with a state setter, however.
+            
         } else {
             //tell the user how it actually should be spelled.
             const [firstCard, ...remainingCards] = cards;
@@ -117,6 +132,7 @@ export function PracticePage() {
     //redirect when all the cards have been iterated through.
     //most likely that we cannot say "cards.length === 0" immediately,
     //because...
+    //do it on the entire sentence.
     return (
         <>
             <p>{cards[0]?.word}, {cards[0]?.romaji}</p>
